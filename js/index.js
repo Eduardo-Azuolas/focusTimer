9c72c79a0929e63f7c452c095a2cd28e1014a8ea
Eduardo Azuolas <eduardoazuolas@gmail.com>
2022-10-26 15:05:30 -0300

    Finalizando a modularização do projeto

--- js/index.js
+++ js/index.js
@@ -3,17 +3,16 @@
 // Default Import
 import Controls from "./controls.js"
 import Timer from "./timer.js"
-
-// DOM || Document Object Model
-
-const buttonPlay = document.querySelector('.play')
-const buttonPause = document.querySelector('.pause')
-const buttonStop = document.querySelector('.stop')
-const buttonSet = document.querySelector('.set')
-const buttonSoundOn = document.querySelector('.sound-on')
-const buttonSoundOff = document.querySelector('.sound-off')
-const minutesDisplay = document.querySelector('.minutes')
-const secondsDisplay = document.querySelector('.seconds')
+import Sound from "./sounds.js"
+import Events from "./events.js"
+import {
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  buttonSet,
+  minutesDisplay,
+  secondsDisplay
+} from "./elements.js"
 
 const controls = Controls({
   buttonPause,
@@ -29,43 +28,6 @@ const timer = Timer({
   resetControls: controls.reset
 })
 
-// Event-driven
-// Programação imperativa
-// Callback
+const sound = Sound()
 
-buttonPlay.addEventListener('click', function() {
-  controls.play()
-  timer.countdown()
-})
-
-buttonPause.addEventListener('click', function() {
-  controls.pause()
-  timer.hold()
-})
-
-buttonStop.addEventListener('click', function() {
-  controls.reset()
-  timer.reset()
-})
-
-buttonSoundOff.addEventListener('click', function() {
-  buttonSoundOn.classList.remove('hide')
-  buttonSoundOff.classList.add('hide')
-})
-
-buttonSoundOn.addEventListener('click', function() {
-  buttonSoundOn.classList.add('hide')
-  buttonSoundOff.classList.remove('hide')
-})
-
-buttonSet.addEventListener('click', function() {
-  let newMinutes = controls.getMinutes()
-
-  if (!newMinutes) {
-    timer.reset()
-    return
-  }
-
-  timer.updateDisplay(newMinutes, 0)
-  timer.updateMinutes(newMinutes)
-})
+Events({controls, timer, sound})
